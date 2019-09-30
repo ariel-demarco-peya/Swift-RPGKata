4af2426fd9769f2da743227e9c3df5806e16aa68
Ariel-Demarco-Peya <ariel.demarco@pedidosya.com>
2019-09-30 11:30:01 -0300

    Initial Commit

---
+++ RPG/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RPG
+//
+//  Created by Ariel Demarco on 30/09/2019.
+//  Copyright © 2019 Ariel Demarco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
